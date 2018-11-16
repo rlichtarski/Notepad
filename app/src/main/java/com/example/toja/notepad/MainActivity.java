e76d32e5fee4cbec83d8a2de684ec0c3969a74df
rradzzio <32550649+rradzzio@users.noreply.github.com>
2018-11-16 16:24:19 +0100

    set onclicklistener in fab

--- app/src/main/java/com/example/toja/notepad/MainActivity.java
+++ app/src/main/java/com/example/toja/notepad/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.toja.notepad;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
