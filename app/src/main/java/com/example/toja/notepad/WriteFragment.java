62ccdbf9510193352c36b94ba992708e3c6677eb
rradzzio <32550649+rradzzio@users.noreply.github.com>
2018-11-17 13:58:33 +0100

    added rootView in onCreateView method in WriteFragment

--- app/src/main/java/com/example/toja/notepad/WriteFragment.java
+++ app/src/main/java/com/example/toja/notepad/WriteFragment.java
@@ -40,8 +40,10 @@ public class WriteFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_write,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_write,container,false);
+
+        
+        return rootView;
     }
 
 }
