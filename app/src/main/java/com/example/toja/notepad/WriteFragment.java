22eda90d95ce61a08c8a5301d2b385e633ebd0f3
rradzzio <32550649+rradzzio@users.noreply.github.com>
2018-11-17 15:16:23 +0100

    added margin to edittext in fragment_write.xml

--- app/src/main/java/com/example/toja/notepad/WriteFragment.java
+++ app/src/main/java/com/example/toja/notepad/WriteFragment.java
@@ -26,7 +26,6 @@ public class WriteFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //if(savedInstanceState == null) mEditTextValue = "";
     }
 
     @Override
