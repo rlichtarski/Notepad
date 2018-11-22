b5cd6e42e348f474dd9eb67ec136acef0c5bd3e4
rradzzio <32550649+rradzzio@users.noreply.github.com>
2018-11-22 18:01:42 +0100

    changed the style of AlertDialog to Theme Material Dialog Alert

--- app/src/main/java/com/example/toja/notepad/WriteFragment.java
+++ app/src/main/java/com/example/toja/notepad/WriteFragment.java
@@ -2,6 +2,7 @@ package com.example.toja.notepad;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
@@ -82,12 +83,21 @@ public class WriteFragment extends DialogFragment {
     }
 
     private void showAlertDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(getActivity(), android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(getActivity());
+        }
+
         builder.setMessage(R.string.save_your_changes)
                 .setPositiveButton(R.string.save,new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface,int i) {
                         Toast.makeText(getActivity(), "Saved", Toast.LENGTH_SHORT).show();
+
+                        //TODO: save the file
+
                         WriteFragment.this.dismiss();
                     }
                 })
