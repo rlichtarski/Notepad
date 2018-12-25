d8d9c6f34f95a21a2302b6ec2d70bd21c5638a28
rradzzio <32550649+rradzzio@users.noreply.github.com>
2018-12-25 22:50:30 +0100

    deleted unneccessary lines of code from the Note model

--- app/src/main/java/com/example/toja/notepad/database/model/Note.java
+++ app/src/main/java/com/example/toja/notepad/database/model/Note.java
@@ -8,45 +8,9 @@ public class Note {
     public static final String COL_NOTE = "NOTE";
     public static final String COL_DATE = "DATE";
 
-    private int mId;
-    private String mNote;
-    private String mDate;
-
     public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
             + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
             + COL_NOTE + " TEXT,"
             + COL_DATE + " TEXT"
             + ");";
-
-    public Note(int id, String note, String date) {
-        this.mId = id;
-        this.mNote = note;
-        this.mDate = date;
-    }
-
-    public int getId() {
-        return mId;
-    }
-
-    public void setId(int mId) {
-        this.mId = mId;
-    }
-
-    public String getNote() {
-        return mNote;
-    }
-
-    public void setNote(String mNote) {
-        this.mNote = mNote;
-    }
-
-    public String getDate() {
-        return mDate;
-    }
-
-    public void setDate(String mDate) {
-        this.mDate = mDate;
-    }
-
-
 }
